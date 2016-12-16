7adbac246620e9e32bd50149a8aedd09d8199ca6
Julio <juliowebdeveloper@hotmail.com>
2016-12-16 11:24:51 -0200

    Finish SavedInstance

--- app/src/main/java/com/sriyanksiddhartha/fragmentdemo/FragmentA.java
+++ app/src/main/java/com/sriyanksiddhartha/fragmentdemo/FragmentA.java
@@ -8,10 +8,17 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class FragmentA extends Fragment {
 
 	private static final String TAG = FragmentA.class.getSimpleName();
+
+    private Button button;
+    private TextView textView;
+    int score;
 
 	@Nullable
 	@Override
@@ -19,8 +26,45 @@ public class FragmentA extends Fragment {
 
 		View view = inflater.inflate(R.layout.fragment_a, container, false);
 		Log.i(TAG, "onCreateView()");
-
-		return view;
+            button = (Button) view.findViewById(R.id.button);
+            textView = (TextView) view.findViewById(R.id.textView);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    textView.setText("Dummy Text");
+                button.setText("Logout");
+                    score=45;
+                Toast.makeText(getActivity(), "Score : "+ score, Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view;
 	}
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        if(savedInstanceState!=null){
+            score = savedInstanceState.getInt("SCORE");
+        }else {
+            score = 0;
+        }
+        Toast.makeText(getActivity(), "Score: "+ score, Toast.LENGTH_SHORT).show();
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putInt("SCORE", score);
+        super.onSaveInstanceState(outState);
+        Log.i(TAG, "onSaveInstance");
+    }
 }
